3325d7c3db147b5f41b252fa01041ef9bb7319df
Jothin Kumar <contact@jothin.tech>
2024-01-15 23:14:25 +0530

    overflow anchor was the impostor!
    https://stackoverflow.com/a/43001201/15923012

--- assets/script/abt.js
+++ assets/script/abt.js
@@ -1,4 +1,4 @@
-let scrollBoost = 3;
+const scrollBoost = 3;
 window.scrollBoost = true; // To disable scroll boost set this to false.
 
 function bodyLoadedAbt() {
@@ -7,12 +7,9 @@ function bodyLoadedAbt() {
     const handler = () => {
         if (window.scrollY > p & window.scrollBoost) { // If scrolled down
             let y = p + (window.scrollY - p)*scrollBoost;
-            window.scrollTo(window.scrollX, y < abt.scrollHeight ? y: abt.scrollHeight);
+            window.scroll(window.scrollX, y < abt.scrollHeight ? y: abt.scrollHeight);
             window.removeEventListener("scroll", handler);
             window.addEventListener("scroll", handler2);
-            if (y > abt.scrollHeight) {
-                scrollBoost = 1;
-            }
         }
         p = window.scrollY;
     }
