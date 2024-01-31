cd3abec23a38017366e557de340f09026d113398
Jothin Kumar <contact@jothin.tech>
2024-01-31 23:23:10 +0530

    individual pages for slides

--- build.sh
+++ build.sh
@@ -1,3 +1,18 @@
+cp build-config.json _build-config.json  #
+
+if [ -d "slides-git" ]
+then
+    cd slides-git
+    git pull
+    cd ../
+else
+    git clone https://github.com/Jothin-kumar/jothin.tech-slides.git slides-git
+fi
+printf "git (slides) ✅\n\n"
+
+python3 s-generate.py
+printf "slides generate ✅\n\n"
+
 if [ -d "build" ]
 then
     cd build
@@ -12,3 +27,5 @@ python3 build/build.py
 printf "build.py ✅\n\n"
 cp -r assets/images build-output/img
 printf "copy images ✅\n\n"
+
+cp _build-config.json build-config.json  #
